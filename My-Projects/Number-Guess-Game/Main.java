bcd2734bfaa16e3b45bf9d20b04b745f8315d678
ahmetbahaakturk <ahmetbahaakturk1@outlook.com>
2023-08-02 18:03:25 +0300

    NumberGuessGame(fixing text and font)

--- My-Projects/Number-Guess-Game/Main.java
+++ My-Projects/Number-Guess-Game/Main.java
@@ -3,27 +3,30 @@ import java.util.Random;
 
 public class Main {
 
-    public static int[] loweruppercounter = {0,20,10};
-    public static int[] temploweruppercounter = loweruppercounter;
+    public static int[] lowerUpperCounter = {0, 20, 10};
+    public static int[] tempLowerUpperCounter = lowerUpperCounter;
     public static int score = 0;
-    public static int highestscore = 0;
+    public static int highestScore = 0;
 
     public static void main(String[] args) {
         menu();
 
     }
 
-    static void menu(){
-        int menum;
+    static void menu() {
+        int menuNum;
         while (true) {
             System.out.println("1 - Play\n2 - Settings\n3 - Highest Score");
-            menum = getInt();
-            switch (menum){
-                case 1 : play();
+            menuNum = getInt();
+            switch (menuNum) {
+                case 1:
+                    play();
                     break;
-                case 2 : settings();
+                case 2:
+                    settings();
                     break;
-                case 3 : highestscore();
+                case 3:
+                    highestScore();
                     break;
             }
 
@@ -31,60 +34,58 @@ public class Main {
 
     }
 
-    static void play(){
+    static void play() {
         Random random = new Random();
         int answer;
-        loweruppercounter = temploweruppercounter;
-        System.out.println(loweruppercounter[0] + " between " + loweruppercounter[1]);
-        answer = random.nextInt(loweruppercounter[1] - loweruppercounter[0]) + loweruppercounter[0];
+        lowerUpperCounter = tempLowerUpperCounter;
+        System.out.println(lowerUpperCounter[0] + " between " + lowerUpperCounter[1]);
+        answer = random.nextInt(lowerUpperCounter[1] - lowerUpperCounter[0]) + lowerUpperCounter[0];
 
-        while(loweruppercounter[2] > 0){
+        while (lowerUpperCounter[2] > 0) {
 
-                if(answer == getInt()){
-                    System.out.println("True");
-                    loweruppercounter[2]--;
-                    score++;
-                    answer = random.nextInt(loweruppercounter[1] - loweruppercounter[0]) + loweruppercounter[0];
-                }
-                else{
-                    loweruppercounter[2]--;
-                }
+            if (answer == getInt()) {
+                System.out.println("True");
+                lowerUpperCounter[2]--;
+                score++;
+                answer = random.nextInt(lowerUpperCounter[1] - lowerUpperCounter[0]) + lowerUpperCounter[0];
+            } else {
+                lowerUpperCounter[2]--;
+            }
         }
         System.out.println("Your total score is " + score);
-        if(score > highestscore){
-            highestscore = score;
+        if (score > highestScore) {
+            highestScore = score;
         }
         score = 0;
-        loweruppercounter = temploweruppercounter;
+        lowerUpperCounter = tempLowerUpperCounter;
     }
 
-    static void settings(){
+    static void settings() {
         int i = 0;
-        String[] settings = {"Lowerbound:","Upperbound:","Count:"};
-        int[] settingsnum = {0, 0, 0};
+        String[] settings = {"Lower bound:", "Upper bound:", "Count:"};
+        int[] settingsNum = {0, 0, 0};
 
-        while(true){
+        while (true) {
 
-                System.out.print(settings[i]);
-            settingsnum[i] = getInt();
+            System.out.print(settings[i]);
+            settingsNum[i] = getInt();
             i++;
-            if(i == 3){
+            if (i == 3) {
                 break;
             }
         }
-        temploweruppercounter = settingsnum;
+        tempLowerUpperCounter = settingsNum;
     }
 
-    static void highestscore(){
-        System.out.println("Your highest score is " + highestscore);
+    static void highestScore() {
+        System.out.println("Your highest score is " + highestScore);
     }
 
-    public static int getInt(){
+    public static int getInt() {
         Scanner scanner = new Scanner(System.in);
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             return Integer.parseInt(scanner.nextLine());
-        }
-        else{
+        } else {
             System.out.println("Please enter a valid number:");
             return getInt();
         }
