7ca5def285b49110252606a9d87748acc21b627f
ahmetbahaakturk <ahmetbahaakturk1@outlook.com>
2023-08-04 20:57:48 +0300

    Combination-Finder added

---
+++ Java101/Combination-Finder/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n, r;
+
+        System.out.print("(n): ");
+        n = scanner.nextInt();
+
+        System.out.print("(r): ");
+        r = scanner.nextInt();
+
+
+        System.out.println(combination(n, r));
+    }
+
+    /*------------Factorial Function------------*/
+
+    static long factorial(int a) {
+        long factorial = 1L;
+        for (int i = a; i > 0; --i) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    /*------------Combination Function------------*/
+
+    static long combination(int n, int r) {
+        return factorial(n) / (factorial(r) * factorial(n - r));
+    }
+}
