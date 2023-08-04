0747b885558ccfac5a81c874d2b98ae5b4772c9d
ahmetbahaakturk <ahmetbahaakturk1@outlook.com>
2023-08-04 21:11:42 +0300

    Exponentiation added

---
+++ Java101/Exponentiation/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int base, exp;
+
+        System.out.print("Number: ");
+        base = scanner.nextInt();
+
+        System.out.print("Exponent: ");
+        exp = scanner.nextInt();
+
+        System.out.println(exponentiation(base, exp));
+    }
+
+    static int exponentiation(int base, int exp) {
+        int finalNumber = 1;
+        for (int i = 0; i < exp; i++) {
+            finalNumber *= base;
+        }
+        return finalNumber;
+    }
+}
