f72a0d4c4fad4d9be14d1221d26246f9ec26ff77
ahmetbahaakturk <ahmetbahaakturk1@outlook.com>
2023-08-07 23:25:26 +0300

    GCD-ICM fixed

--- Java101/GCD-ICM/src/Main.java
+++ Java101/GCD-ICM/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1, num2, tempNum;
+        int num1, num2;
 
         System.out.print("First Number: ");
         num1 = scanner.nextInt();
