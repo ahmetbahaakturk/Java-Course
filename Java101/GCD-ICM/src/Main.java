35c7aeca01eb6ae64eb5640cc27923fd14f48826
ahmetbahaakturk <ahmetbahaakturk1@outlook.com>
2023-08-07 23:24:37 +0300

    GCD-ICM fixed

--- Java101/GCD-ICM/src/Main.java
+++ Java101/GCD-ICM/src/Main.java
@@ -4,16 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1, num2, tempNum;
+
         System.out.print("First Number: ");
         num1 = scanner.nextInt();
+
         System.out.print("Second Number: ");
         num2 = scanner.nextInt();
-
-        if (num1 > num2) {
-            tempNum = num1;
-            num1 = num2;
-            num2 = tempNum;
-        }
 
         System.out.print("\nGCD: " + GCD(num1, num2) + "\nICM: " + ICM(num1, num2));
     }
