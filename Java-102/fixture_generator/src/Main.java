bd136a5032524eda9ccd5c6b1633e09a024bfcdc
Ahmet Baha Aktürk <ahmetbahaakturk1@outlook.com>
2023-09-26 21:57:17 +0300

    something new

---
+++ Java-102/fixture_generator/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int teamNum = Integer.parseInt(scanner.nextLine());
+        boolean isEven = teamNum % 2 == 0;
+        int roundNum = teamNum * (teamNum - 1) / ((isEven) ? teamNum : teamNum - 1);
+
+        HashMap<>
+
+        for (int i = 0; i < teamNum; i++) {
+
+        }
+    }
+}
